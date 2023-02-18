8aa76dd09e167d15f65eed2f2992616bb6c7ef0c
GriffinMulei <griffinmulei@gmail.com>
2023-02-17 23:16:02 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main -  print number from 0 to 10.
+ * Return: 0 (Run successful).
+ *
+ */
+
+int main(void)
+{
+	int countNumber;
+
+	for (countNumber = 0; countNumber < 10; countNumber++)
+	printf("%d", countNumber);
+	printf("\n");
+
+	return (0);
+}
