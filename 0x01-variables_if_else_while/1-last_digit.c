3ae36044fe46ed2256ecc188b69f4fdb7118c7da
GriffinMulei <griffinmulei@gmail.com>
2023-02-18 00:27:30 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - Printing out the last digit out.
  * Return: 0 (Run successful).
  *
  */
-
 int main(void)
 {
 	int n;
 	int lastNumber;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	lastNumber = n % 10;
