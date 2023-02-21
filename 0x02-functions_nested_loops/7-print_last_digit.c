e7b72fe1967755916ddf78dd70c04b9b2bb95afe
GriffinMulei <griffinmulei@gmail.com>
2023-02-21 12:57:51 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * print_last_digit - prints the last digit
- * @n: the number
- * Return: the last digit
+ `* @n: the number
+ * Return: the last digi
  */
 
 int print_last_digit(int n)
