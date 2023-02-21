68f33ded5bc4d97d35efd17a1a207ab3681a341d
GriffinMulei <griffinmulei@gmail.com>
2023-02-21 10:42:31 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphebet - make the alphebet
+ * Return void
+ */
+void print_alphabet(void)
+{
+	char c;
+	
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
