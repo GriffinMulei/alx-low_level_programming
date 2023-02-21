1d0aa94942e907bb92ecefe231b82cb750bcf8fb
GriffinMulei <griffinmulei@gmail.com>
2023-02-21 12:47:05 -0800

    7-print_last_digit.c;git push

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit
+ * @n: the number
+ * Return: the last digit
+ */
+
+int print_last_digit(int n)
+{
+	int last_digit;
+
+	if (n < 0)
+	{
+		last_digit = (-1 * (n % 10));
+		_putchar (last_digit + '0');
+		return (last_digit);
+	}
+	else
+	{
+		last_digit = (n % 10);
+		_putchar (last_digit + '0');
+		return (last_digit);
+	}
+}
