875469ef31ad6cc708d9fe377c6af02425ced9e1
GriffinMulei <griffinmulei@gmail.com>
2023-02-24 11:47:01 -0800

    o-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,10 +4,13 @@
  * main - Test function for positive or negative
  * Return: 0
  */
+
 int main(void)
 {
- int i;
- i = 0;
- positive_or_negative(i);
- return (0);
+int i;
+
+i = 0;
+positive_or_negative(i);
+
+return (0);
 }
