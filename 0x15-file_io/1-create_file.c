d33d1d6cea54054314e545f9d77534dfad282288
GriffinMulei <griffinmulei@gmail.com>
2023-04-10 06:08:55 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,31 +11,30 @@
  *
  * Rerurn: 1 on success, -1 on any failure
  */
+
 int create_file(const char *filename, char *text_content)
+
 {
-	int fd;
-	int nletters;
-	int rwr;
+	int o, w, len = 0;
 
-	if (!filename)
+
+	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+	o = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	w = write(o, text_content, len);
 
-	if (fd == -1)
+
+	if (o == -1 || w == -1)
 		return (-1);
 
-	if (!text_content)
-		text_content = "";
 
-	for (nletters = 0; text_content[nletters]; nletters++)
-		;
+	close(o);
 
-	rwr = write(fd, text_content, nletters);
-
-	if (rwr == -1)
-		return (-1);
-
-	close(fd);
 	return (1);
 }
