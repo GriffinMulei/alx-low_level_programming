8201161bd92cb7ce9473683c0b28ddd1dab4d462
GriffinMulei <muleigriffin@gmail.com>
2023-06-15 10:25:22 -0700

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,7 +2,7 @@
 
 /*
  * print_dlistint - Prints all elements of a dlistint_t list
- * @h: Pointer to the head of the list
+ * @h: Pointer to the head of the dlistint_t list
  *
  * Return: The number of nodes in the list
  */
