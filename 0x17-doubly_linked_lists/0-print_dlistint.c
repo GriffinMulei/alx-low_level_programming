88fe5e97f95bd728caecd94db9a137e41e5fb941
GriffinMulei <muleigriffin@gmail.com>
2023-06-15 10:18:46 -0700

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,14 +9,13 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t nodes = 0;
+	size_t nodes = 0;
 
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        nodes++;
-    }
-
-    return (nodes);
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nodes++;
+	}
+	return (nodes);
 }
